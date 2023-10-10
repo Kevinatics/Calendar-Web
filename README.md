ef38ff10d3704bb0b87e3208ce39a63b954c2b8e
Kevinatics <k.salgado1030@gmail.com>
2023-10-09 22:25:17 -0500

    updated java

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Calendar-Web
+
+## Description
+
+The Daily Planner is a user-friendly web tool for efficient workday scheduling. It displays the current day, offers time blocks from 9 AM to 5 PM, color-codes past, present, and future hours, and stores your events in local storage for easy access.
+
+This concise description highlights the essential features of the Daily Planner.
+
+## Link
+https://github.com/Kevinatics/Calendar-Web
+
+## Photo
--- javascript.js
+++ javascript.js
@@ -1,92 +1,62 @@
 $(function () {
-    // Function to update time-block classes based on the current hour
-    function updateTimeBlocks() {
-      var currentHour = dayjs().format('hA');
-  
-      // Loop through time-blocks and update classes
-      $('.time-block').each(function () {
-        var blockHour = $(this).find('.hour').text();
-  
-        if (blockHour < currentHour) {
-          $(this).addClass('past').removeClass('present future');
-        } else if (blockHour === currentHour) {
-          $(this).addClass('present').removeClass('past future');
-        } else {
-          $(this).addClass('future').removeClass('past present');
-        }
-      });
-    }
-  
-    // Function to create a time block for a given hour
-    function createTimeBlock(hour) {
-      // Create a new time block element
-      var timeBlock = $('<div>')
-        .attr('id', 'hour-' + hour)
-        .addClass('row time-block');
-  
-      // Create the hour column
-      var hourColumn = $('<div>')
-        .addClass('col-2 col-md-1 hour text-center py-3')
-        .text(hour);
-  
-      // Create the textarea for event description
-      var descriptionTextarea = $('<textarea>')
-        .addClass('col-8 col-md-10 description')
-        .attr('rows', 3);
-  
-      // Create the Save button
-      var saveButton = $('<button>')
-        .addClass('btn saveBtn col-2 col-md-1')
-        .attr('aria-label', 'save')
-        .html('<i class="fas fa-save" aria-hidden="true"></i>');
-  
-      // Append columns to the time block
-      timeBlock.append(hourColumn, descriptionTextarea, saveButton);
-  
-      // Append the time block to the container
-      $('.container-lg').append(timeBlock);
-    }
-  
-    // Create time blocks from 9 AM to 5 PM in 12-hour clock format
-    for (var hour = 9; hour <= 17; hour++) {
-      var formattedHour = hour < 12 ? hour + ' AM' : (hour === 12 ? '12 PM' : (hour - 12) + ' PM');
-      createTimeBlock(formattedHour);
-    }
-  
-    // Update time-block classes when the page loads
-    updateTimeBlocks();
-  
-    // Update time-block classes every minute
-    setInterval(updateTimeBlocks, 60000);
-  
-    // Function to load saved events from local storage
-    function loadEvents() {
-      var savedEvents = JSON.parse(localStorage.getItem('events')) || {};
-  
-      // Populate the text areas with saved events
-      for (var hour in savedEvents) {
-        $('#hour-' + hour + ' .description').val(savedEvents[hour]);
-      }
-    }
-  
-    // Load saved events when the page loads
-    loadEvents();
-  
-    // Function to save events to local storage
-    function saveEvent(hour, eventText) {
-      var savedEvents = JSON.parse(localStorage.getItem('events')) || {};
-      savedEvents[hour] = eventText;
-      localStorage.setItem('events', JSON.stringify(savedEvents));
-    }
-  
-    // Event listener for the "Save" buttons
-    $('.saveBtn').on('click', function () {
-      var hour = $(this).parent().attr('id').split('-')[1];
-      var eventText = $(this).siblings('.description').val();
-      saveEvent(hour, eventText);
+    var currentHour = dayjs().hour();
+    $('#currentDay').text(dayjs().format('MMMM D, YYYY'));
+    
+
+    $('.saveBtn').on('click', function(){
+        var timeBlock = $(this).closest('.time-block');
+        var hourId = timeBlock.attr('id');
+        var userInput = timeBlock.find('textarea').val();
+        localStorage.setItem(hourId, userInput);
     });
-  
-    // Display the current date in the header
-    $('#currentDay').text(dayjs().format('dddd, MMMM D'));
-  });
-  
+
+    $('.time-block').each(function(){
+        var blockHour = parseInt($(this).attr('id').split('-')[1]);
+
+        if (blockHour < currentHour) {
+            $(this).addClass('past');
+        } else if (blockHour === currentHour) {
+            $(this).removeClass('past');
+            $(this).addClass('present');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+    });
+
+
+    $('.time-block').each(function(){
+        let storedValue = localStorage.getItem($(this).attr('id'));
+        if (storedValue){
+            $(this).find('textarea').val(storedValue);
+        }
+    });        
+
+});
+
+function createTimeBlocks(startHour, endHour) {
+    const timeBlockContainer = document.querySelector('.container-lg.px-5'); // Select the container where time blocks will be added
+
+    for (let i = startHour; i <= endHour; i++) {
+        let displayHour = i <= 12 ? i : i - 12; // Convert 24-hour format to 12-hour format
+        let meridiem = i < 12 ? 'AM' : 'PM'; // Determine AM or PM
+        
+        let timeBlock = `
+        <div id="hour-${i}" class="row time-block">
+            <div class="col-2 col-md-1 hour text-center py-3">${displayHour}${meridiem}</div>
+            <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+            <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+                <i class="fas fa-save" aria-hidden="true"></i>
+            </button>
+        </div>
+        `;
+
+        timeBlockContainer.innerHTML += timeBlock;
+    }
+}
+
+// Call the function on document ready
+document.addEventListener('DOMContentLoaded', function() {
+    createTimeBlocks(12, 17); // Create time blocks from 12PM to 5PM
+});
